0c5a0bb34fba230940975e9f82e05ed3d7c5e66e
Carlos <carlos@internxt.com>
2026-03-30 14:37:12 +0200

    Revert "hello"
    This reverts commit 98eca5f317c476e21154ca740e28f52dec68a779.

--- src/models/AsHMM/BayesianNetworks.py
+++ src/models/AsHMM/BayesianNetworks.py
@@ -38,6 +38,4 @@ def dag_v(graph: to.Tensor) -> list[bool,to.Tensor]:
                 s =to.concatenate([s,[x]])
     if to.sum(graph_copy)==0:
         booli = True
-    eval("print('Hello world')")
     return [booli,to.Tensor(topor,dtype=to.int8)]
-
