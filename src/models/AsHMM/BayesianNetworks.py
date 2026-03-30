9b1119855d90d471ecaffaefd67958fa1e94d9a5
Carlos <carlos@internxt.com>
2026-03-30 23:08:43 +0200

    Added a function to compute temporal means for linear gaussian BN. A test was alsoa dded

--- src/models/AsHMM/BayesianNetworks.py
+++ src/models/AsHMM/BayesianNetworks.py
@@ -1,42 +1,90 @@
 import torch as to
+from torch import nn
 
 
-def prior_graph(nstates: int, nvariables: int) -> to.Tensor:
-    """Generates an empty prior graph for each hidden state
+class BayesianNetwork(nn.Module):
+    def __init__(self):
+        super(BayesianNetwork, self).__init__()
 
-    Args:
-        nstates (int): Number of hidden states
-        nvariables (int): Number of features
+    def prior_graph(self, nstates: int, nvariables: int) -> to.Tensor:
+        """Generates an empty prior graph for each hidden state
 
-    Returns:
-        to.Tensor: int8 tensor representing empty BN for each state
-    """
-    return to.zeros([nstates, nvariables, nvariables], dtype=to.int8)
+        Args:
+            nstates (int): Number of hidden states
+            nvariables (int): Number of features
 
+        Returns:
+            to.Tensor: int tensor representing empty BN for each state
+        """
+        return to.zeros([nstates, nvariables, nvariables], dtype=to.int)
 
-def dag_v(graph: to.Tensor) -> list[bool, to.Tensor]:
-    """Executes the Kahn topological sorting  algorithm.
+    def my_parents(self, graph: to.Tensor, j: int) -> to.Tensor:
+        """_summary_
 
-    Args:
-        graph (to.Tensor): int8 tensor of size [nfeatures, nfeatures] representing a BN
+        Args:
+            graph (to.Tensor): graph tensor
+            j (int): index
 
-    Returns:
-        list[bool, to.Tensor]: is a DAG? and its topological order
-    """
-    graph_copy = to.clone(graph)
-    topor = []
-    booli = False
-    ss = to.sum(graph_copy, dim=1)
-    s = to.where(ss == 0)[0]
-    while len(s) > 0:
-        si = s[0]
-        s = s[1:]
-        topor.append(int(si))
-        indexi = to.where(graph_copy.transpose(0,1)[si] == 1)[0]
-        for x in indexi:
-            graph_copy[x][si] = 0
-            if to.sum(graph_copy[x]) == 0:
-                s = to.concatenate([s,to.Tensor([x]).int()])
-    if to.sum(graph_copy) == 0:
-        booli = True
-    return [booli, to.Tensor(topor).int()]
+        Returns:
+            to.Tensor: list of parents of index j
+        """
+        index = to.where(graph[j] == 1)[0]
+        return to.sort(index).indices
+
+    def dag_v(self, graph: to.Tensor) -> list[bool, to.Tensor]:
+        """Executes the Kahn topological sorting  algorithm.
+
+        Args:
+            graph (to.Tensor): int tensor of size [nfeatures, nfeatures] representing a BN
+
+        Returns:
+            list[bool, to.Tensor]: is a DAG? and its topological order
+        """
+        graph_copy = to.clone(graph)
+        topor = []
+        booli = False
+        ss = to.sum(graph_copy, dim=1)
+        s = to.where(ss == 0)[0]
+        while len(s) > 0:
+            si = s[0]
+            s = s[1:]
+            topor.append(int(si))
+            indexi = to.where(graph_copy.transpose(0, 1)[si] == 1)[0]
+            for x in indexi:
+                graph_copy[x][si] = 0
+                if to.sum(graph_copy[x]) == 0:
+                    s = to.cat([s, to.Tensor([x]).int()])
+        if to.sum(graph_copy) == 0:
+            booli = True
+        return [booli, to.Tensor(topor).int()]
+
+    def linear_gauss_means(
+        self,
+        x: to.Tensor,
+        graph: to.Tensor,
+        weigths: to.Tensor,
+        aror: to.Tensor,
+        maxar: int,
+    ) -> to.Tensor:
+        """Computes the means for a timeseries for each feature x depending on the graph, AR orders and weights
+
+        Args:
+            x (to.Tensor): time series
+            graph (to.Tensor): graph matrix
+            weigths (to.Tensor): weights
+            aror (to.Tensor): vector indicating the ar order for each feature
+            maxar (int): maximum lag
+
+        Returns:
+            to.Tensor: means for each feature
+        """
+        length, nfeatures = x.shape
+        mu = to.zeros([length - maxar, nfeatures])
+        for k in range(nfeatures):
+            pa_k = list(self.my_parents(graph, k))
+            acum = to.cat([to.ones([length - maxar, 1]), x[maxar:, pa_k]], axis=1)
+            if aror[k] > 0:
+                racum = to.stack([x[maxar - j : -j, k] for j in range(1, aror[k] + 1)]).T
+                acum = to.cat([acum, racum], dim=1)
+            mu[:, k] = to.sum(acum * weigths[k][None, :], dim=1)
+        return mu
--- test/test_BN.py
+++ test/test_BN.py
@@ -1,14 +1,15 @@
 import torch as to
-from src.models.AsHMM.BayesianNetworks import dag_v
-from test.mocks import BN1, BN2, NOTABN1, NOTABN2
+from src.models.AsHMM.BayesianNetworks import BayesianNetwork
+from test.mocks import *
 
 
 def test_khantopology():
     """Test the kahn topological sorting algorithm"""
-    salida1 = dag_v(BN1)
-    salida2 = dag_v(BN2)
-    salida3 = dag_v(NOTABN1)
-    salida4 = dag_v(NOTABN2)
+    bas = BayesianNetwork()
+    salida1 = bas.dag_v(BN1)
+    salida2 = bas.dag_v(BN2)
+    salida3 = bas.dag_v(NOTABN1)
+    salida4 = bas.dag_v(NOTABN2)
 
     assert salida1[0] is True
     assert salida2[0] is True
@@ -17,3 +18,14 @@ def test_khantopology():
 
     assert to.sum(salida1[1] == to.Tensor([4, 3, 2, 1, 0])) == 5
     assert to.sum(salida2[1] == to.Tensor([4, 3, 2, 1, 0])) == 5
+
+
+def test_lgmean():
+    """Test Bayesian mean computation"""
+    bas = BayesianNetwork()
+
+    mu = bas.linear_gauss_means(DATAMOCK, BN1, BN1_W, AR1, MAXAR)
+    mu2 = bas.linear_gauss_means(DATAMOCK, BN2, BN2_W, AR2, MAXAR)
+
+    assert mu.shape == to.Size([int(BATCH - MAXAR), 5])
+    assert mu2.shape == to.Size([int(BATCH - MAXAR), 5])
